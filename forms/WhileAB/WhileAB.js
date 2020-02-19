0db60c4b91e005450b5524665ab73659e3c319e9
cmc06604 <cmc06604@creighton.edu>
2020-02-19 16:36:56 -0600

    Final Code

---
+++ forms/WhileAB/WhileAB.js
@@ -0,0 +1,13 @@
+/*
+let firstName = prompt("Please give your first name")
+let lastName = prompt("Please give your second name")
+let fullName = (firstName + " " + lastName)
+
+
+i = 0
+while (i < fullName.length) {
+  console.log("Letter " + (i+1) +  " is " + fullName.charAt(i))
+  i++
+}
+
+*/  
