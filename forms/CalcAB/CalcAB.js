a8b1863a976c30c8372d7c916715c55f69f2769e
cmc06604 <cmc06604@creighton.edu>
2020-02-19 17:08:15 -0600

    Final Final code

--- forms/CalcAB/CalcAB.js
+++ forms/CalcAB/CalcAB.js
@@ -11,10 +11,22 @@ let stoppingPoint = Math.floor(myNumbers.length/2)
 
 let products = []
 let j = 0
-for ( i = 0; i < stoppingPoint; i+2) {
-  products[j] = (ProductCalc(myNumbers[i] + myNumbers[i+1]))
-  j = j + 1
+
+//Let user find product of array ONE BY ONE
+for (i = 0; i <= stoppingPoint*2-1; i += 2) { 
+  products[j] = ProductCalc(myNumbers[i], myNumbers[i+1])
+  j = j + 1
+}
+alert(products)
+
+products.push(33874)
+
+let sum = 0;
+
+for(var i = 0; i < products.length; i++){
+
+  sum += products[i]
+
 }
 
-console.log(products)
-  
+alert(sum);
