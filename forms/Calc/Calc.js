912332883046462113dd4c0976412bab8364864e
cmc06604 <cmc06604@creighton.edu>
2020-02-23 17:49:53 -0600

    final

---
+++ forms/Calc/Calc.js
@@ -0,0 +1,11 @@
+function sumCalc(n1,n2){
+  return n1+n2
+  }
+
+num1 = parseInt(prompt("Enter the first number"))
+num2 = parseInt(prompt("Enter another number"))
+
+
+summedNumbers = sumCalc(num1, num2)
+
+alert(`The sum of ${num1} and ${num2} is ${summedNumbers}`)
